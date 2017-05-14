e485fc37b3c153183835330858ffbcc2867e4ed0
masnun3 <masnun3@gmail.com>
2017-05-14 19:38:27 +0600

    Fix shooting dely

--- game.js
+++ game.js
@@ -421,6 +421,7 @@ Ship = function () {
     }
     if (KEY_STATUS.space) {
       if (this.delayBeforeBullet <= 0) {
+	  this.delayBeforeBullet = 10;
         for (var i = 0; i < this.bullets.length; i++) {
           if (!this.bullets[i].visible) {
             SFX.laser();
@@ -539,6 +540,7 @@ BigAlien = function () {
 
     this.delayBeforeBullet -= delta;
     if (this.delayBeforeBullet <= 0) {
+	
       this.delayBeforeBullet = 22;
       for (var i = 0; i < this.bullets.length; i++) {
         if (!this.bullets[i].visible) {
